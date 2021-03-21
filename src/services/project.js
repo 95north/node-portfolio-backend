b610178407e7f8ce2a3920ff12284cf80fabc20b
Tori Sigler <mssigler@gmail.com>
2021-03-21 05:48:23 -0400

    Cleaned up Project Services code

--- src/services/project.js
+++ src/services/project.js
@@ -73,22 +73,10 @@ async function aProject (){
 
 addProject = async (req) => {
     // console.log("Has req.body? req param received in addProj is: ", req.body);  //  req=IncomingMessage { _readableState: ReadableState {
-    //  WORKS !! Get Body: is an array containing a string: '�PNG\r' + '\n\u001a\n\u0000\u0000\u0000\r' +
-
-    // let image = await processImageUpload(req.body[0])
-    // let images = []
-    // let numberOfImages = req.body.length
-    // for (let i=0; i<numberOfImages; i++) {     // is not a function or its return value is not iterable
-    //     let img = await processImageUpload(req.body[i])
-    //     images.push(img)
-    // }    
+    //  WORKS !! Get Body: is an array containing a string: '�PNG\r' + '\n\u001a\n\u0000\u0000\u0000\r' + 
     
     let images = await processImageArray(req)
     console.log("in main addProject, images arr is: ", images )
-
-    // for (var key of req.body.images.entries()) {     // is not a function or its return value is not iterable
-    //     console.log(key[0] + ', ' + key[1]);
-    // }
 
     try {
         let project = new Project({
@@ -100,7 +88,6 @@ addProject = async (req) => {
             "languages": req.body.languages ? req.body.languages : "None !",
             "year": req.body.year ? req.body.year : 2050 ,
             "images": images
-            // "images": req.images
         });
         project["dateAdded"] = new Date();
         let result = await project.save();
@@ -122,126 +109,58 @@ processImageArray = async (req) => {
     let numberOfImages = req.body.length
 
     // console.log("req.body", req.body)      // 'data:image/png;base64,iVBORw0KGgoA ... 
-    console.log("----typeof req.body", typeof req.body)     // Obj
-    console.log("----keys in req.body", Object.keys(req.body) );    // Object éArray Iteratorê äü
-    console.log("----# keys in req.body", Object.keys(req.body).length );    // 1
-    // console.log("req.body[Array]", req.body["Array"] );    // Object éArray Iteratorê äü
-    // let reqBody = JSON.parse(req.body)              //   SyntaxError: Unexpected token d in JSON at position 0
-    // console.log("typeof reqBody (parsed JSON)", typeof reqBody) 
+    // console.log("----typeof req.body", typeof req.body)     // Obj
+    // console.log("----keys in req.body", Object.keys(req.body) );    // Object éArray Iteratorê äü
+    // console.log("----# keys in req.body", Object.keys(req.body).length );    // 1
 
     for (let i=0; i<numberOfImages; i++) {     // is not a function or its return value is not iterable
         let img = await processImageUpload(req.body[i])    /// REFACTOR BODY TO HAVE >1 IMG
         images.push(img)
     }    
     return images
-    
 }
 
 
 
 
 processImageUpload = async (base64Img) => {   // convert base64Img file (str),  to  buffer,  to binary. 
-    let decodedImage = "Default val no good";
-    console.log("processImageUpload - typeof base64Img : ", typeof base64Img)   // string
+    // console.log("processImageUpload - typeof base64Img : ", typeof base64Img)   // string
+    let file = await convertBase64toBuffer(base64Img);   // returns a buffer
 
-
-    let file = await readBase64ImgFile(base64Img);   // returns a buffer
-
-
-
-    // fs.readFile(base64Img, async function(err, base64Img){
-    //     console.log("base64img is: ", base64Img);
-    //     decodedImage = await new Buffer.from(base64Img).toString('binary');
-    //     fs.writeFile('image_decoded.jpg', decodedImage, function(err) {console.log("processImageUpload writefile err: ", err)});
-    // });
     console.log("processImageUpload - typeof file : ", typeof file)  // obj
     console.log("processImageUpload - file instanceof Buffer  : ", file instanceof Buffer )  // obj
     console.log("processImageUpload - file instanceof Binary (testing atob(base64str); )  : ", file instanceof Binary )  // obj
-
     console.log("processImageUpload - file length : ", file.length)  // string
-
 
     var imageBin = {};
     imageBin.bin = await  Binary(file);                 // const Binary = require('mongodb').Binary;
-    // imageBin.bin = await  Binary(decodedImage);                 // const Binary = require('mongodb').Binary;
-
 
     console.log("imageBin.bin is instance of Binary? : ", imageBin.bin instanceof Binary) // true
-    console.log("imageBin.bin is  : ", imageBin.bin )   //object
     console.log("imageBin.bin KEYS is  : ", Object.keys(imageBin.bin) )   //object
-    // console.log("imageBin.bin[0] is  : ", imageBin.bin["0"] )   // undefined
     console.log("imageBin.bin[0] is  : ", imageBin.bin["buffer"] )   // WORKS
-
-
     console.log("typeof imageBin.bin is  : ", typeof imageBin.bin )
 
-    // console.log("imageBin.bin.Binary.buffer is  : ", imageBin.bin[0].buffer )
-
-    // SHOULD BE: imageBin.bin is: = �PNG
+    return imageBin.bin["buffer"]
+    // in Mongoose.js file,  imageBin.bin is: = �PNG
     // IHDR�{���
     //          %iCCPICC ProfileH���TS���$$$�@����^�R	$���YTp-��`EWD]
     // ��������R@W�{������?��e2w�                                      �
-    //                           �1l�(�G�'�
-    // dNLNa�X;6G"
-    // ���P����wס5�+�2_?��W���$��i\	'�Ap7�H����f� a�@[
+    //                           �1l�(�G�'�                                     AND SO ON
 
+    // Vs. here: imageBin.bin is  :  Binary ä
+    //     buffer: <Buffer 89 50 ......
+    // STill works. 
     
-    return imageBin.bin["buffer"]
-    // What gets inserted into MongoDB: 
+    // What gets inserted into MongoDB here: 
     //  images: [ 'BinData(0, ' + '"iVBORw0KGgoAAAANSUhEUgAABmwAAAQoCAYAAAAHVfHnAAAMJWlDQ1BJQ0MgUHJvZmlsZQAASImVlwdUU8kagOeWJCQktEAEpITeBJFepNcIAlIFGyEJJJQYEoKKHVlUcC2oiGBFV0....
 }       // ends  processImageUpload
 
 
-readBase64ImgFile = (base64Img) => {   // returns a buffer
-    
-    let decodedImage = null;
-
+convertBase64toBuffer = (base64Img) => {   // returns a buffer
     let buff = new Buffer(base64Img.slice(21), 'base64');   // data:image/png;base64,  remove 1st 22 chars. 
-    decodedImage = buff.toString('ascii');
-
-    
-    
-    // fs.readFile(base64Img, async function(base64Img, err){  // base64img is:  éError: ENAMETOOLONG: name too long, open 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAA
-    
-    // fs.readFile(base64Img, 'base64', async function(err, base64Img2){  // base64img is:  éError: ENAMETOOLONG: name too long, open 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAA
-    // //  The "path" argument must be one of type string, Buffer, or URL. Received type object
-    // // fs.readFile(image_origial, function(err, original_data){
-        //     // // This tells node to load the file into a Buffer 'original_data' because you
-        //     // // have not specified an encoding for the returned values. If you provided an
-        //     // // encoding, then original_data would be a string with that encoding.
-        
-        //     console.log("base64img is: ", base64Img2);    // undefined!!!!! 
-        //     decodedImage = await Buffer.from(base64Img2, 'base64'); // got rid of new keyword
-        //     decodedImage = decodedImage.toString('binary');
-        
-        
-        //     fs.writeFile('image_decoded.jpg', decodedImage, function(err) {console.log("processImageUpload writefile err: ", err)});
-        // });
-        
-        // readFile expects a file path...
-        // decodedImage = fs.readFile(base64Img,  async function (err, data) {
-            // decodedImage = fs.readFile(base64Img, {encoding: 'base64'}, async function (err, data) {   // errno: -63, code: 'ENAMETOOLONG',
-            //     console.log ("decodedImage err is: ", err)
-            //  The "path" argument must be one of type string, Buffer, or URL. Received type object
-            // fs.readFile(image_origial, function(err, original_data){
-                // // This tells node to load the file into a Buffer 'original_data' because you
-                // // have not specified an encoding for the returned values. If you provided an
-                // // encoding, then original_data would be a string with that encoding.
-                
-                // console.log("base64img is: ", base64Img2); // undefined!!!!! 
-                // decodedImage = await Buffer.from(base64Img2, 'base64'); // got rid of new keyword
-                // decodedImage = decodedImage.toString('binary');
-                
-                
-                // fs.writeFile('image_decoded.jpg', decodedImage, function (err) { console.log("processImageUpload writefile err: ", err); });
-                
-                // });
-                // atob(): decodes a base64 encoded string("atob" should be read as "ASCII to binary").
-                // return atob(base64Img);   // NOPE. returns neither Binary nor Buffer typeof. 
-
-                return buff;
-                return decodedImage;
-            }
+    // decodedImage = buff.toString('ascii');
+    return buff;
+}
             
             
             
